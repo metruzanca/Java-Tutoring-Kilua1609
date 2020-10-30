0349daff7ad4848f90f11568b3c4dfa1d04fb39b
Metruzanca - gamingpc <metruzanca@gmail.com>
2020-10-30 18:20:49 +0100

    Boilerplate for flow control exercise

---
+++ src/java/FlowControl.java
@@ -0,0 +1,16 @@
+public class FlowControl {
+  public static void main(String[] args) {
+
+  }
+
+  public static int getMax(int[] list) {
+
+    return 0;
+  }
+
+  public static int getMin(int[] list) {
+
+    return 0;
+  }
+
+}
