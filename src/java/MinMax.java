8f7045426ce7384b91e26fa79688ca2b3bcfedfd
Mattia Collari <mattia.collari@studio.unibo.it>
2020-11-02 20:28:32 +0100

    TestoEsMAx

--- src/java/MinMax.java
+++ src/java/MinMax.java
@@ -1,12 +1,28 @@
 public class MinMax {
   public static void main(String[] args) {
-    int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+    int[] numbers = new int[] { 1, 2, 3, 4, 5 };  //non so quanta roba c'è
     int max = getMax(numbers);
     System.out.println(max); // 5
   }
 
   public static int getMax(int[] arrayNumeri) {
+    int i = 0;
+  if(arrayNumeri[0] > arrayNumeri[i]) {
+
+    }
+  
+    }
+  }
 
     return 0;
+  }
+
+  public static int compareNum(int a , int b) {
+    if(a < b) {
+      return b;
+    } else {
+      return a;
+    }
+    
   }
 }
