003291cb93115177c3cbc9a07ef85b95602362eb
Mattia Collari <mattia.collari@studio.unibo.it>
2020-11-12 19:03:10 +0100

    esercizzi

---
+++ src/java/EsercizioMovie.java
@@ -0,0 +1,25 @@
+public class EsercizioMovie {
+    private String title;
+    private String studio;
+    private String rating;
+
+    public EsercizioMovie(String title, String studio, String rating) {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+    public EsercizioMovie (String title, String studio, String rating) {
+        this.title = title;
+        this.studio = studio;
+        this.rating = "PG";
+    }
+
+    /***
+     * creare un ciclo per mettere a confronto a due d due gli elementi dell'array
+     * porre cosa succede se gli array dei film hanno entrambi pg
+     */
+
+     public static String[] getPG()
+
+}
